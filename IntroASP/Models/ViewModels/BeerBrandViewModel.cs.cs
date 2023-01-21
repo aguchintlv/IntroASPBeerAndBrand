0b8b3052fba1919f98b8c32d8abca16fbe435e02
Aguchin <aguchintlv@gmail.com>
2023-01-20 20:40:46 -0500

    first commit

---
+++ IntroASP/Models/ViewModels/BeerBrandViewModel.cs.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroASP.Models.ViewModels
+{
+    public class BeerBrandViewModel
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+
+
+    }
+}
